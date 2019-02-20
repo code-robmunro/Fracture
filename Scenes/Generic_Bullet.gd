c452eb3bd951f32aab21c54eb26ec9de61bf8fbe
Robert Munro <code.robmunro@gmail.com>
2019-02-20 00:58:00 -0800

    Add 10mm bullet graphics, add basic shooting, add bullet cooldown and clamping

---
+++ Scenes/Generic_Bullet.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+var origin
+var target
+const BULLET_SPEED = 1000
+# signal hit(body, damage)
+
+func _ready():
+    self.look_at(get_viewport().get_mouse_position())
+    target = get_viewport().get_mouse_position() - self.position
+    origin = self.position
+    
+func free():
+    queue_free()
+
+func _physics_process(delta):
+    global_translate(target.normalized() * BULLET_SPEED * delta)
+    if origin.distance_to(position) > Constants.PROJECTILE_MAX_DISTANCE:
+        free()
--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -1,10 +1,13 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
+var Generic_Bullet = preload("res://Scenes/Generic_Bullet.tscn")
 
 var velocity = Vector2()
 var previous_direction = Vector2()
 var previous_animation_direction = "down"
+var do_shoot
+var mouse_position = Vector2()
+const BULLET_MAX = 10
 
 func get_input():
     previous_direction = velocity
@@ -22,20 +25,18 @@ func get_input():
         velocity.x += 1
     if Input.is_action_pressed('down'):
         velocity.y += 1
+        
+    if Input.is_mouse_button_pressed(BUTTON_LEFT):
+        if $bullet_cooldown.is_stopped():
+            do_shoot = true
+            
 
-    return
-    
 func update_animation():
     if previous_direction.x != velocity.x or previous_direction.y != velocity.y:
         $animation.stop()
         start_animation()
-    elif $animation.is_playing():
-        pass
-    else:
-        start_animation()
+    
         
-    return
-
 func start_animation():
     assert($animation.is_playing() == false)
     if velocity == Vector2.UP:
@@ -52,12 +53,24 @@ func start_animation():
         previous_animation_direction = "down"
     else:
         $animation.play("player_idle_" + previous_animation_direction)
-        
-    return
 
-func _physics_process(delta):
+
+func shoot():
+    if do_shoot == true:
+        var parent = get_parent()
+        var bullet = Generic_Bullet.instance()
+        bullet.position = position
+        parent.add_child(bullet)
+        $bullet_cooldown.start()
+    do_shoot = false
+    
+
+func _process(delta):
     get_input()
     update_animation()
+    shoot()
+
+
+func _physics_process(delta):
     move_and_slide(velocity.normalized() * Constants.ENTITY_BASE_SPEED)
-    
-    return
+
